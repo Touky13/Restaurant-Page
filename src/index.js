c79cc5d65c01e6c55254e2728d71d42936f42c2a
Guillaume <197660339+Touky13@users.noreply.github.com>
2025-10-06 13:52:30 +0200

    Added tabs logic

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 console.log("Hello World!");
 homepage();
-menu();
+
+const container = document.querySelector("#content");
+const homepageBtn = document.querySelector("#homepage");
+const menuBtn = document.querySelector("#menu");
+
+menuBtn.addEventListener("click", () => {
+    container.innerHTML = "";
+    menu();
+});
+
+homepageBtn.addEventListener("click", () => {
+    container.innerHTML = "";
+    homepage();
+})
 
 import { homepage } from "./homepage.js";
 import { menu } from "./menu.js";
